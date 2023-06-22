73b65ce774fa25d64478bbfd359af61630ddc507
DESKTOP-B2288JJ\Rafaela <Rafaela@owntec.com.br>
2023-06-22 08:57:02 -0300

    correção js

--- script.js
+++ script.js
@@ -1,31 +1,32 @@
-const altura = document.querySelector("#altura");
-const peso = document.querySelector("#peso");
-const resposta = document.querySelector("#calcularBtn");
+let altura = document.querySelector("input#altura");
+let peso = document.querySelector("input#peso");
+let resposta = document.querySelector("#valor");
 
-const calcularIMC = () => {
-  const imc = (peso.value / (altura.value * altura.value)).toFixed();
+function calcularIMC () {
+  let peson = Number(peso.value);
+  let alturan = Number(altura.value);
+  let imc = ( peson / alturan**2).toFixed(2)
   let valorImc = "";
-  console.log(imc);
 
   if (imc < 18.5) {
-    valorImc = `IMC = ${imc} - Você está abaixo do peso.`;
+    valorImc = `Seu IMC é ${imc} - Você está abaixo do peso.`;
   } else if (imc <= 25) {
-    valorImc = `IMC = ${imc} - Você está com o peso normal.`;
+    valorImc = `Seu IMC é ${imc} - Você está com o peso normal.`;
   } else if (imc <= 30) {
-    valorImc = `IMC = ${imc} - Você está com sobrepeso.`;
+    valorImc = `Seu IMC é ${imc} - Você está com sobrepeso.`;
   } else if (imc <= 35) {
-    valorImc = `IMC = ${imc} - Você está com obesidade grau I.`;
+    valorImc = `Seu IMC é ${imc} - Você está com obesidade grau I.`;
   } else if (imc <= 40) {
-    valorImc = `IMC = ${imc} - Você está com obesidade grau III.`;
+    valorImc = `Seu IMC é ${imc} - Você está com obesidade grau III.`;
   } else {
-    valorImc = `IMC = ${imc} - Você está com obesidade grau III.`;
+    valorImc = `Seu IMC é ${imc} - Você está com obesidade grau III.`;
   }
 
   resposta.textContent = valorImc;
 };
 
-// function calcularIMC() {
-//   const imc = (peso / (altura * altura)).toFixed(2);
+
+// teste botão range para o input de peso:
 
 // function rangeButton() {
 //     let resultado = document.getElementById("resultado");
