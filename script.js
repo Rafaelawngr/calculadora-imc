845e75e9c93f7f0d9a6d0de96dc6b9d4a68201e3
Rafaela Ellwanger <107516309+Rafaelawngr@users.noreply.github.com>
2023-06-21 21:00:39 -0300

    ajuste responsividade e js

--- script.js
+++ script.js
@@ -1,40 +1,40 @@
-// 4 - dentro colocar as condicionais de imc
-// 5 - exibir o resultado puxando pelo innerHTML
+const altura = document.querySelector("#altura");
+const peso = document.querySelector("#peso");
+const resposta = document.querySelector("#calcularBtn");
 
+const calcularIMC = () => {
+  const imc = (peso.value / (altura.value * altura.value)).toFixed();
+  let valorImc = "";
+  console.log(imc);
 
+  if (imc < 18.5) {
+    valorImc = `IMC = ${imc} - Você está abaixo do peso.`;
+  } else if (imc <= 25) {
+    valorImc = `IMC = ${imc} - Você está com o peso normal.`;
+  } else if (imc <= 30) {
+    valorImc = `IMC = ${imc} - Você está com sobrepeso.`;
+  } else if (imc <= 35) {
+    valorImc = `IMC = ${imc} - Você está com obesidade grau I.`;
+  } else if (imc <= 40) {
+    valorImc = `IMC = ${imc} - Você está com obesidade grau III.`;
+  } else {
+    valorImc = `IMC = ${imc} - Você está com obesidade grau III.`;
+  }
 
-const altura = document.querySelector('#altura');
-const peso = document.querySelector('#peso');
-const resposta = document.getElementById('resposta');
+  resposta.textContent = valorImc;
+};
 
-function calcularIMC() {
-
-    const imc = (peso / (altura*altura)).toFixed(2);
-  
-        if (imc < 18.5) {
-        resposta.innerHTML = `IMC = ${imc} - Você está abaixo do peso.`
-        } else if (imc <= 25) {
-            //Normal
-        } else if (imc <= 30) {
-            //Sobrepeso
-        } else if (imc <= 35) {
-            //Obesidade grau I
-        } else if (imc <= 40) {
-            //Obesidade grau II
-        } else {
-            //Obesidade grau III
-        }
-    }
-
+// function calcularIMC() {
+//   const imc = (peso / (altura * altura)).toFixed(2);
 
 // function rangeButton() {
 //     let resultado = document.getElementById("resultado");
 //     let valor = document.getElementById("peso").value;
 //     resultado.innerHTML = valor
 //    }
-   
+
 //    rangeButton()
-   
+
 //    document.addEventListener("change", range);
 
 // if (isNaN(altura) || isNaN(peso)) {
